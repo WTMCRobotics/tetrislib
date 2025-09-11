c45376c94ed4e8ce70df05400e3b58a9918d01dc
alloyedWaterfowl <ryeschersten@gmail.com>
2025-09-11 16:35:11 -0400

    Update greeting message in Main class

--- app/src/main/java/com/wtmcrobotics/tetris/Main.java
+++ app/src/main/java/com/wtmcrobotics/tetris/Main.java
@@ -2,6 +2,6 @@ package com.wtmcrobotics.tetris;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, WTMCRobotics!");
+        System.out.println("Hello, Strange Quarks!");
     }
 }
