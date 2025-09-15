45ef4db3717eb6e338ba67b466a4777b182e8edf
xlumen1 <proxyig74@gmail.com>
2025-09-15 22:31:03 +0000

    Added GridRenderer

---
+++ app/src/main/java/com/wtmcrobotics/tetris/graphics/GridRenderer.java
@@ -0,0 +1,47 @@
+package com.wtmcrobotics.tetris.graphics;
+import javax.swing.*;
+import java.awt.*;
+
+public class GridRenderer extends JPanel {
+    private final int gridSizeX;
+    private final int gridSizeY;
+    private final int cellSize;
+    private int[][] board; // [[ROW], [ROW], [ROW], ...]
+
+    public GridRenderer(int gridSizeX, int gridSizeY, int cellSize) {
+        this.gridSizeX = gridSizeX;
+        this.gridSizeY = gridSizeY;
+        this.cellSize = cellSize;
+        this.board = new int[gridSizeY][gridSizeX]; // Initialize with empty board
+        setPreferredSize(new Dimension(gridSizeX * cellSize, gridSizeY * cellSize));
+    }
+
+    private final Color[] colors = {
+            Color.BLACK, // 0 - empty
+            Color.CYAN,  // 1 - I
+            Color.YELLOW,// 2 - O
+            Color.MAGENTA,// 3 - T
+            Color.GREEN, // 4 - S
+            Color.RED,   // 5 - Z
+            Color.BLUE,  // 6 - J
+            Color.ORANGE // 7 - L
+    };
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int y = 0; y < gridSizeY; y++) {
+            for (int x = 0; x < gridSizeX; x++) {
+                int cellValue = board[y][x];
+                g.setColor(colors[cellValue]);
+                g.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+                g.setColor(Color.DARK_GRAY);
+                g.drawRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            }
+        }
+    }
+    
+    public void setBoard(int[][] board) {
+        this.board = board;
+    }
+}
--- app/src/main/java/com/wtmcrobotics/tetris/graphics/Renderer.java
+++ app/src/main/java/com/wtmcrobotics/tetris/graphics/Renderer.java
@@ -1,12 +1,23 @@
-package com.wtmcrobotics.graphics;
+package com.wtmcrobotics.tetris.graphics;
+import javax.swing.*;
+import java.awt.*;
+import GridRenderer;
 
 class Renderer {
 	public int grid_size_x;
 	public int grid_size_y;
-	int[][] board;
+	int[][] board; // [[ROW], [ROW], [ROW], ...]
+	private GridRenderer gridRenderer;
 	public Renderer(int grid_size_x, int grid_size_y) {
 		this.grid_size_x = grid_size_x;
 		this.grid_size_y = grid_size_y;
+		gridRenderer = new GridRenderer(grid_size_x, grid_size_y, 30);
+		board = new int[grid_size_y][grid_size_x]; // Initialize with empty board
+		JFrame frame = new JFrame("Tetris");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(gridRenderer);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	public void set_board(int[][] board) {
@@ -14,6 +25,7 @@ class Renderer {
 	}
 
 	public void render() {
-		//TODO: Do this
+		gridRenderer.setBoard(board);
+		gridRenderer.repaint();
 	}
 }
