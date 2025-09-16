0c0236096986da1d04e78a049c0914e585876c90
xlumen1 <proxyig74@gmail.com>
2025-09-16 15:03:41 +0000

    Remove all demo code Add javadocs to exposed class

--- app/src/main/java/com/wtmcrobotics/tetris/graphics/Renderer.java
+++ app/src/main/java/com/wtmcrobotics/tetris/graphics/Renderer.java
@@ -1,11 +1,19 @@
 package com.wtmcrobotics.tetris.graphics;
 import javax.swing.*;
 
+/**
+ * Renderer class that contains all rendering logic.
+ */
 public class Renderer {
 	public int grid_size_x;
 	public int grid_size_y;
 	int[][] board; // [[ROW], [ROW], [ROW], ...]
 	private GridRenderer gridRenderer;
+	/**
+	 * Initializes the renderer with a grid of given size
+	 * @param grid_size_x Number of columns
+	 * @param grid_size_y Number of rows
+	 */
 	public Renderer(int grid_size_x, int grid_size_y) {
 		this.grid_size_x = grid_size_x;
 		this.grid_size_y = grid_size_y;
@@ -18,10 +26,17 @@ public class Renderer {
 		frame.setVisible(true);
 	}
 
+	/**
+	 * Sets the board state to be rendered
+	 * @param board 2D array representing the board state as an array of rows
+	*/
 	public void set_board(int[][] board) {
 		this.board = board;
 	}
 
+	/**
+	 * Pushes the current board state to the renderer and repaints
+	 */
 	public void render() {
 		gridRenderer.setBoard(board);
 		gridRenderer.repaint();
